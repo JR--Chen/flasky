bb5ed461ba138a73547aeffa700d89b87e554050
JR--Chen <13359852063@163.com>
2017-11-01 09:24:32 +0800

    api v1.0(test) support

--- app/__init__.py
+++ app/__init__.py
@@ -32,9 +32,11 @@ def create_app(config_name):
     from .main import main as main_blueprint
     from .auth import auth as auth_blueprint
     from .wechat import wechat as wechat_blueprint
+    from .api_1_0 import api as api_1_0_blueprint
     app.register_blueprint(main_blueprint)
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
     app.register_blueprint(wechat_blueprint, url_prefix='/wechat')
+    app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1.0')
 
     return app
 
---
+++ app/api_1_0/errors.py
@@ -0,0 +1,28 @@
+from flask import jsonify, request, g
+from . import api
+from ..models import Post
+from .. import db
+from ..exceptions import ValidationError
+
+
+def bad_request(message):
+    response = jsonify({'error': 'bad request', 'message': message})
+    response.status_code = 400
+    return response
+
+
+def unauthorized(message):
+    response = jsonify({'error': 'unauthorized', 'message': message})
+    response.status_code = 401
+    return response
+
+
+def forbidden(message):
+    response = jsonify({'error': 'forbidden', 'message': message})
+    response.status_code = 403
+    return response
+
+
+@api.errorhandler(ValidationError)
+def validation_error(e):
+    return bad_request(e.arg[0])
